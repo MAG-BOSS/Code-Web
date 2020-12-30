0a22cfcd96a458f707a21c1908ead12489912afa
MAG-BOSS <b18025@students.iitmandi.ac.in>
2020-12-30 14:59:54 +0530

    fixed iframe related issue and added styling to it

--- src/App/App.js
+++ src/App/App.js
@@ -2,29 +2,36 @@ import React,{useState,useEffect} from "react";
 import {useDebounce} from "use-debounce";
 
 import Editor from "../components/Editor/Editor";
-import "./App.css";
+ import "./App.css";
 
 const App = ()=>{
-    const [html, sethtml]=useState(null);
-    const [css, setcss]=useState(null);
-    const [js,setjs]=useState(null);
+    const [htmlVal, sethtml]=useState(null);
+    const [cssVal, setcss]=useState(null);
+    const [jsVal,setjs]=useState(null);
     const [output,setOutput]=useState("");
 
-    const dhtml=useDebounce(html,1000);
-    const dcss=useDebounce(css,1000);
-    const djs=useDebounce(js,1000);
+    const dhtml=useDebounce(htmlVal,250);
+    const dcss=useDebounce(cssVal,250);
+    const djs=useDebounce(jsVal,250);
 
     
     useEffect(()=>{
-        const out = `<html>
-        <style>
-        ${dcss}
-        </style>
+        const out = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Document</title>
+          <style>
+            ${dcss}
+          </style>
+        </head>
         <body>
-        ${dhtml}
-        <script type="text/javascript">
-        ${djs}
-        </script>
+          ${dhtml}
+          <script type="text/javascript">
+            ${djs}
+          </script>
         </body>
         </html>`;
         setOutput(out);
@@ -36,13 +43,13 @@ const App = ()=>{
                 <h1>CODEWEB</h1>
             </div>
             <div className="editor-pane">
-                <Editor className="html-code" type="HTML" value={html} mode="htmlmixed" onBeforeChange={(editor,data,html)=>{sethtml(html)}}/>
-                <Editor className="css-code" type="CSS" value={css} mode="css" onBeforeChange={(editor,data,css)=>{setcss(css)}}/>
-                <Editor className="js-code" type="JS" value={js} mode="javascript" onBeforeChange={(editor,data,js)=>{setjs(js)}}/>
+                <Editor className="html-code" type="HTML" value={htmlVal} mode="htmlmixed" onBeforeChange={(editor,data,htmlVal)=>{sethtml(htmlVal)}}/>
+                <Editor className="css-code" type="CSS" value={cssVal} mode="css" onBeforeChange={(editor,data,cssVal)=>{setcss(cssVal)}}/>
+                <Editor className="js-code" type="JS" value={jsVal} mode="javascript" onBeforeChange={(editor,data,jsVal)=>{setjs(jsVal)}}/>
             </div>
             <div className="bottom-pane">
-                <p>Hello</p>
-                
+                <h1 className="result-header">RESULT</h1>
+                <iframe srcDoc={output} title="Output"/>
             </div>
         </div>
        

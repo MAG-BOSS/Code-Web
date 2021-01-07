c8aa48e1ad420c63735c9bec43aa9aedcede90c1
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-07 16:17:49 +0530

    dark mode init

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from "react";
 import {useDebounce} from "use-debounce";
 
 import Editor from "../components/Editor/Editor";
+import Page from "../components/Questions/Page";
 import "./App.css";
 
 const App = ()=>{
@@ -41,15 +42,22 @@ const App = ()=>{
         <div className="app">
             <div className="app-header">
                 <h1>CODEWEB</h1>
+                <label class="switch">
+                  <input type="checkbox"/>
+                  <span class="slider round"></span>
+                </label>
             </div>
             <div className="editor-pane">
                 <Editor className="html-code" type="HTML" value={htmlVal} mode="htmlmixed" onBeforeChange={(editor,data,htmlVal)=>{sethtml(htmlVal)}}/>
                 <Editor className="css-code" type="CSS" value={cssVal} mode="css" onBeforeChange={(editor,data,cssVal)=>{setcss(cssVal)}}/>
                 <Editor className="js-code" type="JS" value={jsVal} mode="javascript" onBeforeChange={(editor,data,jsVal)=>{setjs(jsVal)}}/>
             </div>
-            <div className="bottom-pane">
+            <div className="result-pane">
                 <h1 className="result-header">RESULT</h1>
                 <iframe srcDoc={output} title="Output"/>
+            </div>
+            <div className="question-pane">
+                <Page/>
             </div>
         </div>
        

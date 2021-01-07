34da4ce2d240667151aed389f2cb85441fff5876
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-07 16:17:00 +0530

    added question pane

---
+++ src/components/Questions/Page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "./Header";
+
+const Page = ()=>{
+    return(
+        <div>
+            <Header/>
+            Hello Page
+        </div>
+    )
+}
+
+export default Page;

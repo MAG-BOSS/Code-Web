8a7d69670cdc4f8ecc2f08d7de8d1adad20247b3
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-08 16:27:46 +0530

    added  user tasks

---
+++ src/components/Questions/tasks/Tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+    {   id:1,
+        src:`<html> <head> Hello </head> <body> Hi Guys </body> </html>`,
+        description:"Replicate the web page",
+        maxScore:10
+    },
+    {
+        id:2,
+        src:`<html><body>Keep it up</body></html>`,
+        description:"Replcate it",
+        maxScore:10
+    }
+
+];
+
+export const getTasks=() => {
+    return tasks;
+}

e76b340d017c9952b9c203cf3b3093abb000ad69
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-10 16:45:40 +0530

    fixes: editor collapse issue

--- src/App/App.js
+++ src/App/App.js
@@ -17,24 +17,7 @@ const App = ()=>{
 
     
     useEffect(()=>{
-        const out = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width" initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Document</title>
-          <style>
-            ${dcss}
-          </style>
-        </head>
-        <body>
-          ${dhtml}
-          <script type="text/javascript">
-            ${djs}
-          </script>
-        </body>
-        </html>`;
+        const out = `<html><head><title>Document</title><style>${dcss}</style></head><body>${dhtml}<script type="text/javascript">${djs}</script></body></html>`;
         setOutput(out);
     },[dhtml,dcss,djs]);
 

af1870756590b6b36edaebb970d4193aabb563e2
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-19 15:27:49 +0530

    added unsolved questions display feature

--- client/src/components/Questions/Page.js
+++ client/src/components/Questions/Page.js
@@ -1,15 +1,24 @@
 import React,{useEffect, useState} from "react";
+import { useSelector } from "react-redux";
 
 import {getTasks} from "../Questions/tasks/Tasks";
 import "./Page.css";
 
 const Page = ()=>{
     const [userChoice, setUserChoice] = useState(1);
+    const userId = (useSelector(state => state.auth.user)).id;
+    const userData = useSelector(state => state.user);
+    const submittedQuestions = (userData.find(p => p._id == userId)).solved_questions;
+    console.log(submittedQuestions);
     
     const handleChange = (e)=>{
         setUserChoice(e.target.value);
     }
+    const questionsToBeDisplayed = (givenData) =>{
+        return !submittedQuestions.includes(Tasks.id);
+    };
     const Tasks = getTasks();
+    const filterTasks = Tasks.filter(questionsToBeDisplayed);
     return(
         <div className="page">
             <div className="header">
@@ -18,12 +27,12 @@ const Page = ()=>{
                     <form>
                         <label>Choose :</label>
                             <select name="qid" id="userChoice" onChange={handleChange} >
-                            {(Tasks).map(( prop ) => (<option value={prop.id}>{prop.id} | maxScore: {prop.maxScore}</option>))}
+                            {(filterTasks).map(( prop ) => (<option value={prop.id}>{prop.id} | maxScore: {prop.maxScore}</option>))}
                             </select>
                     </form>
                  </div>
                  <div className="box">
-	                <a className="button" href="#popup1">Description</a>
+	                <a className="button" href="#popup1">Describe</a>
                 </div>
                 <div id="popup1" className="overlay">
 	                <div className="popup">
